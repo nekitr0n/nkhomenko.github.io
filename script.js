1e00b20b794af92e15c8db5b8e0f58d1e4b6423a
nekitr0n <j5black049@gmail.com>
2024-12-09 18:41:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+function newTodo() {
+  alert('New TODO button clicked!')
+}
